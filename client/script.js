af70fe6c5928cacc1cec1eed0c3982c8404d360d
Drew <drew@Drews-MacBook-Pro.local>
2018-10-07 06:47:32 -0400

    added indicators for when the user is recording or not

--- client/script.js
+++ client/script.js
@@ -1,20 +1,22 @@
-const record_button = getElementById("record_button");
-const stop_button = getElementById("stop_button");
-const recording_text = getElementById("recording_text");
-const recording_animation = getElementById("recording_animation");
+const record_button = document.getElementById("record_button");
+const stop_button = document.getElementById("stop_button");
+const recording_text = document.getElementById("recording_text");
+const recording_animation = document.getElementById("recording_animation");
 
-var now_recording = false;
 
 function start_recording() {
-    if now_recording === true {
-        continue;
-    }
-    else {
-        recording_text.toggle(hidden);
-
+    if (recording_animation.style.animationPlayState != "running") {
+        recording_animation.style.animationPlayState = "running";
+        recording_text.toggleAttribute('hidden');
     }
 }
 
+function stop_recording() {
+    if (recording_animation.style.animationPlayState == "running") {
+        recording_animation.style.animationPlayState = "paused";
+        recording_text.toggleAttribute('hidden');
+    }
+}
 
 record_button.addEventListener("click", start_recording);
 stop_button.addEventListener("click", stop_recording);
